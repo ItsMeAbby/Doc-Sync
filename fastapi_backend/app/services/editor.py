14032f45d727529fb16ec575ac8b85047c7074c0
Muhammad Abdullah Hayat <malik_muhammad222@outlook.com>
2025-06-30 12:33:31 +0200

    completed teh edit api for endpoint

--- fastapi_backend/app/services/editor.py
+++ fastapi_backend/app/services/editor.py
@@ -1,14 +1,45 @@
+from app.services.agents.edit_suggestion_agent import EditAgentResponse, edit_suggestion_agent
+from app.services.agents.editor_agent import edit_content_agent, Edits
 from app.services.agents.intent_detection_agent import Detected_Intent, intent_agent
+
 from agents import Runner
 class MainEditor:
     def __init__(self, query: str):
         self.query = query
+        self.output_chnages={}
 
     async def run(self):
         """
         Run the agent to get the response based on the query.
         """
-        intent= await self._detect_intent()
+        detected_intents= await self._detect_intent()
+        for intent in detected_intents.intents:
+            print(f"Detected intent: {intent.intent} with reason: {intent.reason}")
+            if intent.intent == "edit":
+                # If the intent is to edit, we can run the edit suggestion agent
+                edit_suggestion_agent_response = await Runner.run(edit_suggestion_agent,
+                    f"Suggest edits for the query: {self.query}"
+                )
+                edit_agent_suggestions = edit_suggestion_agent_response.final_output_as(EditAgentResponse)
+                print("Edit Suggestions:")
+                print(edit_agent_suggestions.model_dump_json(indent=2))
+                edit_content_agent_response = await Runner.run(edit_content_agent,
+                    edit_agent_suggestions.model_dump_json(indent=2)
+                )
+                documents_edits= edit_content_agent_response.final_output_as(Edits)
+                print("Edit Content Agent Response:")
+                print(documents_edits.model_dump_json(indent=2))
+                dict_documents_edits = documents_edits.model_dump()
+                self.output_chnages["edit"] = dict_documents_edits.get("changes", [])
+
+
+
+
+
+            
+        return self.output_chnages
+
+        
         
     
     async def _detect_intent(self) -> Detected_Intent:
